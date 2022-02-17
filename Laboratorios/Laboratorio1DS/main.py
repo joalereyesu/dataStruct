80589565e54c357aa61c30a5abd23cfd9c509704
joalereyesu <61555348+joalereyesu@users.noreply.github.com>
2022-02-16 18:36:04 -0600

    lab1

---
+++ Laboratorios/Laboratorio1DS/main.py
@@ -0,0 +1,38 @@
+from typing import Optional
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
+
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/username/{username}")
+async def read_item(username: str):
+    return {"username": username}
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
+
+
+@app.get("/items/{item_id}")
+async def create_item(item_id: int, item: Item):
+    return {"item_id": item_id, **item.dict()}
